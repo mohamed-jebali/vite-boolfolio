26ff1746542e02b094928b2a805d5aea03db863f
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-08-30 13:17:54 +0200

    init scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import AppMain from './components/AppMain.vue'
+export default{
+  name:'App',
+  components:{
+    AppMain,
+  }
+
+}
+</script>
+
+<template>
+  <AppMain/>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss' as * ;
+@import "./bootstrap/scss/bootstrap";
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default{
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center">
+        Main
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

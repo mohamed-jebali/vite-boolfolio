7424d15cc37b8416a5135e961473f26b9cca322d
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-08-31 15:17:39 +0200

    installed router and added pages folder

---
+++ src/components/pages/AppHome.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h1>
+            HomePage
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    
+</style>
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppHome from './components/pages/AppHome.vue'
+import AppProjectList from './components/pages/AppProjectsList.vue'
+import AppProjectCard from './components/pages/AppProjectCard.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes : [
+        {
+            path: '/',
+            name : 'home',
+            component : AppHome,
+        },
+        {
+            path: '/projects',
+            name : '/projects.index',
+            component : AppProjectList,
+        },
+        {
+            path: '/projects/:slug',
+            name : 'projects.show',
+            component : AppProjectCard,
+        },
+    ]});
+
+export{router};
